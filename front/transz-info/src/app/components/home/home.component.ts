3c7d519aaa4023fcedf2bf4d82824a254be56a2d
Muráncsik Adéna <adena.murancsik@gmail.com>
2023-08-27 15:03:34 +0200

    hu/Detranzicio

--- front/transz-info/src/app/components/home/home.component.ts
+++ front/transz-info/src/app/components/home/home.component.ts
@@ -21,7 +21,7 @@ export class HomeComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.titleService.setTitle('Transz Infó');
+    this.titleService.setTitle('Gender Úikalauz');
   }
 
   searchClicked(): void {

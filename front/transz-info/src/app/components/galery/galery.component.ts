d29481a9ab2ea5d297f69018850d447b38573c83
Muráncsik Adéna <adena.murancsik@gmail.com>
2023-08-28 23:24:53 +0200

    Galery initials

--- front/transz-info/src/app/components/galery/galery.component.ts
+++ front/transz-info/src/app/components/galery/galery.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {GaleryService} from "../../services/galery.service";
+import * as myGlobals from "../../globals";
+declare let process: any;
 
 @Component({
   selector: 'app-galery',
@@ -8,8 +10,17 @@ import {GaleryService} from "../../services/galery.service";
 })
 export class GaleryComponent implements OnInit {
   public galery: any[] = [];
+  public baseUrl: string;
 
-  constructor(private galeryService: GaleryService) { }
+  constructor(private galeryService: GaleryService) {
+    const env = process.env.NODE_ENV;
+
+    if (env  === 'production') {
+      this.baseUrl = myGlobals.URL_LIVE;
+    } else {
+      this.baseUrl = myGlobals.URL_DEV;
+    }
+  }
 
   ngOnInit(): void {
     this.galeryService.getGalery()

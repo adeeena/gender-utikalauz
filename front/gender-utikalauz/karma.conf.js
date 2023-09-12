ea2d25149bcb8ef55f4b681f6994deb5071d247c
Muráncsik Adéna <adena.murancsik@gmail.com>
2023-09-12 13:19:34 +0200

    Removed old project names.

--- front/gender-utikalauz/karma.conf.js
+++ front/gender-utikalauz/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/transz-info'),
+      dir: require('path').join(__dirname, './coverage/gender-utikalauz'),
       subdir: '.',
       reporters: [
         { type: 'html' },

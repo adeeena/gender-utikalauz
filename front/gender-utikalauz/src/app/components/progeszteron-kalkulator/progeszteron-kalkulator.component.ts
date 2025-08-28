a9a6ed6bd771c1cff99990103a085bac807119d2
Adéna Auróra Muráncsik <adenam@silversea.com>
2025-08-28 15:01:49 +0200

    chore: Migration from ang v13->v20 Run ng update @angular/material@17

--- front/gender-utikalauz/src/app/components/progeszteron-kalkulator/progeszteron-kalkulator.component.ts
+++ front/gender-utikalauz/src/app/components/progeszteron-kalkulator/progeszteron-kalkulator.component.ts
@@ -1,7 +1,5 @@
 import {Component, HostListener, Inject, OnInit, Renderer2, ViewChild, ElementRef, AfterViewInit} from '@angular/core';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
-import {MatLegacyInputModule as MatInputModule} from '@angular/material/legacy-input';
-import {MatFormFieldModule} from '@angular/material/form-field';
 import {fadeInUpOnEnterAnimation} from "angular-animations";
 import {TranslateService} from "@ngx-translate/core";
 import {DialogElementsExampleDialog} from '../entry/entry.component';

297758f6b6c4b8213ad9b138703c6008040dc8a4
Montefiori Adéna Auróra <adena.montefiori@gmail.com>
2025-08-28 22:54:13 +0200

    chore: pushing vercel api as endpoint

--- sources/src/app/globals.ts
+++ sources/src/app/globals.ts
@@ -1,4 +1,5 @@
 'use strict';
 
 export const URL_DEV='http://localhost:3000';
-export const URL_LIVE='https://api.genderutikalauz.hu';
+//export const URL_LIVE='https://api.genderutikalauz.hu';
+export const URL_LIVE='https://gender-utikalauz-pages.vercel.app';
